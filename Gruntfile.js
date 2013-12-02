b7c9f4fb9dad9e8a541c8e6af0047a463c5f49ce
Darren Hurley <darren.hurley@guardian.co.uk>
2013-12-02 16:51:50 +0000

    updating README

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+/* global module: false, process: false */
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        jslint: {
+            config: {
+                src: ['frontsapi/config/**/*.json']
+            }
+        },
+
+        s3: {
+            options: {
+                bucket: 'aws-frontend-store',
+                access: 'public-read'
+            },
+            config: {
+                sync: [{
+                    src: 'frontsapi/config/**/*.json',
+                    dest: '<%= process.env.ENVIRONMENT ? process.env.ENVIRONMENT.toUpperCase() : "DEV" %>/frontsapi/config/',
+                    rel : 'frontsapi/config'
+                }]
+            }
+        }
+
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-s3');
+    grunt.loadNpmTasks('grunt-jslint');
+
+    // Tasks
+    grunt.registerTask('upload', ['jslint:config', 's3:config']);
+};
